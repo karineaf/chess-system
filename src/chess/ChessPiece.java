e2305e2b969b98129caba6feb27b6898e4de25e2
karine <karineantunesfarias@gmail.com>
2021-03-26 23:54:36 -0300

    adicionado peao

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -12,6 +12,7 @@ public abstract class ChessPiece extends Piece {
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
+        moveCount = 0;
     }
 
     public Color getColor() {
